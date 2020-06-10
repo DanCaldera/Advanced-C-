a566d378050b8bb216f6edf107464a3c1a46c37e
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-06-09 23:21:39 -0500

    added first-program

--- FundamentalConcepts/first-program.cpp
+++ FundamentalConcepts/first-program.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
+    int favorite_number;
     cout << "Enter your favorite number between 1 and 100." << endl;
+    cin >> favorite_number;
+    cout << "Your favorite number is " << favorite_number << "! Mine too!" << endl;
     return 0;
 }
