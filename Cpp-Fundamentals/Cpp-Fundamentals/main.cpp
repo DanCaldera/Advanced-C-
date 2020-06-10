72c2408383125464348a2f1b55e5e20ba8cee0f1
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-06-09 22:57:20 -0500

    added cpp fundamentals project

---
+++ Cpp-Fundamentals/Cpp-Fundamentals/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Cpp-Fundamentals
+//
+//  Created by Brayan Caldera on 09/06/20.
+//  Copyright © 2020 Brayan Caldera. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
