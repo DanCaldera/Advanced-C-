8c41d37134a46e3d4faabf466f86c6de38041247
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-06-10 22:44:46 -0500

    updated study of cpp

---
+++ FundamentalConcepts/Structure/BasicIO/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World!" << endl;
+
+    // cout << "hello!";
+    // cout << " world!" << endl;
+
+    // cout << "Hello world!" << endl;
+    // cout << "Hello"
+    //      << " world!" << endl;
+    // cout << "Hello"
+    //      << " world!\n";
+
+    int num1;
+    int num2;
+    double num3;
+
+    cout << "Enter an integer: ";
+    cin >> num1;
+    cout << "You entered: " << num1 << endl;
+
+    return 0;
+}
---
+++ FundamentalConcepts/Structure/Comments/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // This is a comment
+    /*
+        This too
+    */
+    return 0;
+}
