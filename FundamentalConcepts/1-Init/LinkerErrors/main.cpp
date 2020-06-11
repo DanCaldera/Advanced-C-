95369d469b2ba2d2b5632a225c01d8132ce04e25
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-06-10 21:17:20 -0500

    added init folder

---
+++ FundamentalConcepts/1-Init/LinkerErrors/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+extern int x;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << x << endl;
+    return 0;
+}
---
+++ FundamentalConcepts/1-Init/RuntimeErrors/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 0 / 0 << endl;
+    // File not found
+    // Out of memory
+    return 0;
+}
